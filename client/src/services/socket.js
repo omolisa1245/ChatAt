7165c07aab15ab7ec2265e99ceae85135dc6e960
olayemi omolisa <olayemiomolisa@yahoo.com>
2026-05-28 21:54:29 +0100

    adding url

--- client/src/services/socket.js
+++ client/src/services/socket.js
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-export const socket = io("http://localhost:5000");
+export const socket = io("http://https://chat-at-gold.vercel.app");
 
 socket.on("connect", () => {
   console.log("Socket connected:", socket.id);
--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "http://localhost:5000/api";
+const BASE_URL = "https://chat-at-gold.vercel.app/api";
 
 export const syncUserWithBackend = async (token, userData = {}) => {
   try {
